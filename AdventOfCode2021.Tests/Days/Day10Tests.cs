7e30465282cae6ff4c36e25f3b30bf9074bffaf2
DorijanH <dhaspl@gmail.com>
2021-12-12 20:18:41 +0100

    Day 10

---
+++ AdventOfCode2021.Tests/Days/Day10Tests.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode2021.Days;
+using Xunit;
+
+namespace AdventOfCode2021.Tests.Days;
+
+/// <summary>
+/// Advent day 10 tests class.
+/// </summary>
+public class Day10Tests : TestBase
+{
+    /// <summary>
+    /// Advent day 10 tests class constructor.
+    /// </summary>
+    public Day10Tests() : base(new Day10(true))
+    {
+    }
+
+    /// <summary>
+    /// Tests that the result of the first part of the puzzle is the same as the provided example's result.
+    /// </summary>
+    [Fact]
+    protected override void FirstPart()
+    {
+        Assert.Equal("26397", this.AdventDay.SolveFirstPart());
+    }
+
+    /// <summary>
+    /// Tests that the result of the second part of the puzzle is the same as the provided example's result.
+    /// </summary>
+    [Fact]
+    protected override void SecondPart()
+    {
+        Assert.Equal("288957", this.AdventDay.SolveSecondPart());
+    }
+}
---
+++ AdventOfCode2021/Days/Day10.cs
@@ -0,0 +1,121 @@
+﻿namespace AdventOfCode2021.Days;
+
+/// <summary>
+/// Class representing the advent day 10.
+/// </summary>
+public class Day10 : AdventDay
+{
+    private readonly string[] input;
+
+    private readonly Dictionary<char, char> matchingCharacters = new()
+    {
+        { '(', ')' }, { '[', ']' }, { '{', '}' }, { '<', '>' }
+    };
+    private readonly Dictionary<char, int> syntaxErrorScoring = new()
+    {
+        { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 }
+    };
+    private readonly Dictionary<char, int> autocompleteScoring = new()
+    {
+        { ')', 1 }, { ']', 2 }, { '}', 3 }, { '>', 4 }
+    };
+
+    /// <summary>
+    /// Initializes the class representing the advent day 10.
+    /// </summary>
+    /// <param name="isExample">Should example input be used.</param>
+    public Day10(bool isExample = false) : base(10, isExample)
+    {
+        this.input = this.InputContents
+            .Split('\n', StringSplitOptions.TrimEntries);
+    }
+
+    /// <summary>
+    /// Solves the first part of the advent day puzzle.
+    /// </summary>
+    /// <returns>Puzzle result.</returns>
+    public override string SolveFirstPart()
+    {
+        var syntaxErrorScore = 0;
+
+        foreach (var line in this.input)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var character in line)
+            {
+                // If the character is one of the opening characters, push it to the stack
+                if (this.matchingCharacters.ContainsKey(character))
+                {
+                    stack.Push(character);
+                }
+                else
+                {
+                    // If not, check if it is the closing version of the latest character on the stack
+                    // If not, that's the illegal character
+                    if (this.matchingCharacters[stack.Pop()] != character)
+                    {
+                        syntaxErrorScore += this.syntaxErrorScoring[character];
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"Total syntax error score is: {syntaxErrorScore}");
+
+        return $"{syntaxErrorScore}";
+    }
+
+    /// <summary>
+    /// Solves the second part of the advent day puzzle.
+    /// </summary>
+    /// <returns>Puzzle result.</returns>
+    public override string SolveSecondPart()
+    {
+        var completionScoring = new List<long>();
+
+        foreach (var line in this.input)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var character in line)
+            {
+                // If the character is one of the opening characters, push it to the stack
+                if (this.matchingCharacters.ContainsKey(character))
+                {
+                    stack.Push(character);
+                }
+                else
+                {
+                    // If not, check if it is the closing version of the latest character on the stack
+                    // If not, that's the corrupted line, skip it.
+                    if (this.matchingCharacters[stack.Pop()] != character)
+                    {
+                        stack.Clear();
+                        break;
+                    }
+                }
+            }
+
+            // The line is incomplete (stack is not empty, there are missing closing characters)
+            if (stack.Count != 0)
+            {
+                var score = 0L;
+
+                while (stack.Count > 0)
+                {
+                    score = score * 5 + this.autocompleteScoring[this.matchingCharacters[stack.Pop()]];
+                }
+
+                completionScoring.Add(score);
+            }
+        }
+
+        var sortedCompletionScoring = completionScoring.OrderByDescending(s => s).ToList();
+        var middleScore = sortedCompletionScoring[sortedCompletionScoring.Count / 2];
+
+        Console.WriteLine($"The middle score is: {middleScore}");
+
+        return $"{middleScore}";
+    }
+}
